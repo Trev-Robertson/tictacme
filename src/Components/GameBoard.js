9a666ec3ddfd502b76ecad1c0ef70a8336289c88
Trevahr Robertson <Trevahr@gmail.com>
2020-04-12 23:59:54 -0400

    first draft done!

--- src/Components/GameBoard.js
+++ src/Components/GameBoard.js
@@ -9,28 +9,48 @@ export const GameBoard = () => {
     const [moveNumber, setMoveNumber] = useState(0)
 
     const handleClick = (index) =>{
+        if (!calculateWinner()){
         const updatedGameBoard = [...gameBoardState]
        
         updatedGameBoard[index] = marker
+
         markSquare(updatedGameBoard)
         setMarker( marker === 'X' ? 'O' : 'X')
         let newMoveNumber = moveNumber + 1
         setMoveNumber(newMoveNumber)
+        }
     }
 
     
     
     
-    // const calculateWinner = () => {
-   
+    const calculateWinner = () => {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+          ];
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (gameBoardState[a] && gameBoardState[a] === gameBoardState[b] && gameBoardState[a] === gameBoardState[c]) {
+            //   return [gameBoardState[a], lines[i]];
+            return [true, gameBoardState[a], lines[i]]
+            }
+          }
+          return null;
 
-    // }
+    }
 
+    const gameStatus = calculateWinner()
 
     const reset = () => {
-
         setMoveNumber(0)
-
+        setMarker('X')
         resetBoard()
     }
 
@@ -40,28 +60,29 @@ export const GameBoard = () => {
     return (
         
         <div>
+            <h1>Ultimate Tic Tac Toe</h1>
         <div>
-            {moveNumber < 9 ? <h1>Current Player: {marker}</h1> : <h1>End Game</h1>}
+    {moveNumber >= 9 ? <h2>End Game</h2> : calculateWinner() ? <h2>Player {`${calculateWinner()[1]}`} is the Winner! </h2> : <h2>Current Player: {marker}</h2>}
         </div>
         <div>
             <button className='reset-button' onClick={reset}>
-                    Reset Button
+                    Reset Game
             </button>
         </div>
             <div className='center'>
-             <GameSquare index={0} marker={gameBoardState[0]} handleClick={handleClick}/>
-             <GameSquare index={1} marker={gameBoardState[1]} handleClick={handleClick}/>
-             <GameSquare index={2} marker={gameBoardState[2]} handleClick={handleClick}/>
+             <GameSquare index={0} marker={gameBoardState[0]} handleClick={handleClick} gameStatus={gameStatus}/>
+             <GameSquare index={1} marker={gameBoardState[1]} handleClick={handleClick} gameStatus={gameStatus}/>
+             <GameSquare index={2} marker={gameBoardState[2]} handleClick={handleClick} gameStatus={gameStatus}/>
             </div>
             <div className='center'>
-             <GameSquare index={3} marker={gameBoardState[3]} handleClick={handleClick}/>
-             <GameSquare index={4} marker={gameBoardState[4]} handleClick={handleClick}/>
-             <GameSquare index={5} marker={gameBoardState[5]} handleClick={handleClick}/>
+             <GameSquare index={3} marker={gameBoardState[3]} handleClick={handleClick} gameStatus={gameStatus}/>
+             <GameSquare index={4} marker={gameBoardState[4]} handleClick={handleClick} gameStatus={gameStatus}/>
+             <GameSquare index={5} marker={gameBoardState[5]} handleClick={handleClick} gameStatus={gameStatus}/>
             </div>
             <div className='center'>
-             <GameSquare index={6} marker={gameBoardState[6]} handleClick={handleClick}/>
-             <GameSquare index={7} marker={gameBoardState[7]} handleClick={handleClick}/>
-             <GameSquare index={8} marker={gameBoardState[8]} handleClick={handleClick}/>
+             <GameSquare index={6} marker={gameBoardState[6]} handleClick={handleClick} gameStatus={gameStatus}/>
+             <GameSquare index={7} marker={gameBoardState[7]} handleClick={handleClick} gameStatus={gameStatus}/>
+             <GameSquare index={8} marker={gameBoardState[8]} handleClick={handleClick} gameStatus={gameStatus}/>
             </div>
         </div>
     )
--- src/Components/GameSquare.js
+++ src/Components/GameSquare.js
@@ -1,11 +1,21 @@
 import React from 'react'
 // import {GlobalContext} from '../Context/GlobalState'
 
-export const GameSquare = ({index, handleClick, marker}) => {
+export const GameSquare = ({index, handleClick, marker, gameStatus}) => {
+    
+    
+    const winner = () => {
+        if (gameStatus){
+         return gameStatus[2].some(value => value === index)
+        }
+    }
+    
+
+
 
     return (
-        <div>
-            <button className='button' onClick={marker === '' ? () => handleClick(index) : null }>
+        <div >
+            <button className='button' onClick={marker === '' ? () => handleClick(index) : null } style={{backgroundColor: winner() ? 'green' : null }}>
             <h1>{marker}</h1>
             </button>
         </div>
